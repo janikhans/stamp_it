6d0b57ad45f5bad3b31108aa40ca4d1c0ea8b702
Janik Knittle <janik.knittle@gmail.com>
2016-06-24 21:11:04 -0700

    Remove turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :stamps do
     resources :bets, only: [:create, :edit, :update, :destroy, :new]
   end
-  
+
   resources :bets, only: [:index]
 
   resources :dashboard, only: [:index]
