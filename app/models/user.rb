90bf1b96fa198759b9668e2f4ea185238804f7c3
Janik Knittle <janik.knittle@gmail.com>
2016-06-26 19:59:40 -0700

    Setup login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,31 @@ class User < ApplicationRecord
   has_many :bets
   has_many :payouts
 
+  validates :username, uniqueness: { case_sensitive: false}, presence: true, length: { in: 4..20}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255}, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validate :validate_username
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      conditions.permit! if conditions.class.to_s == "ActionController::Parameters" ##Look into this more
+      where(conditions).first
+    end
+  end
+
   def worth
     payouts.map(&:amount).reduce(0, :+)
   end
+
+  private
+
+    def validate_username
+      if User.where(email: username).exists?
+        errors.add(:username, :invalid)
+      end
+    end
 end
